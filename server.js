68256b2d4d6bed83fb6b3ed34815f9c8cf9a3e30
Borreill <inesborreill@yahoo.fr>
2022-04-19 18:30:33 +0200

    add schema

--- server.js
+++ server.js
@@ -1,14 +1,10 @@
 const PORT = 4000
 const express = require("express");
 const { graphqlHTTP } = require('express-graphql')
+const schema = require("./schemas/index");
 const app = express();
 
-app.use(
-    '/graphql',
-    graphqlHTTP({
-        graphiql: true,
-    }),
-);
+app.use('/graphql', graphqlHTTP({ schema, graphiql: true }))
 
 app.listen(PORT, () => {
     console.log(`Server up and running on port http://localhost:${PORT} 🎉`)
