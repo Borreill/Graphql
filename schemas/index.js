5ccc4cceb7024274e93d463bf25c1e6e0131eaaa
Borreill <inesborreill@yahoo.fr>
2022-04-19 18:42:54 +0200

    add userschema

--- schemas/index.js
+++ schemas/index.js
@@ -1,23 +1,41 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 
 const {
     GraphQLSchema,
     GraphQLObjectType,
-    GraphQLString
+    GraphQLString,
+    GraphQLInt,
 } = graphql
 
-const schema = new GraphQLSchema({
-    query: new GraphQLObjectType({
-        name: 'RootQueryType',
-        fields: {
-            hello: {
-                type: GraphQLString,
-                resolve() {
-                    return 'world'
-                },
-            },
-        },
-    }),
-})
+const users = [
+    { id: '1', firstName: 'Antoine', lastName: 'Laframboise', age: 34, email: "pafihudewo11-50@yopmail.com" },
+    { id: '2', firstName: 'Honore', lastName: 'Lagrange', age: 45, email: "jonukleva3569@yopmail.com" }
+];
+const UserType = new GraphQLObjectType({
+    name: 'User',
+    fields: {
+        id: { type: GraphQLString },
+        firstName: { type: GraphQLString },
+        lastName: { type: GraphQLString },
+        age: { type: GraphQLInt },
+        email: { type: GraphQLString },
+    }
+});
 
-module.exports = schema 
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        user: {
+            type: UserType,
+            args: { id: { type: GraphQLString } },
+            resolve(parentValue, args) {
+                return _.find(users, { id: args.id });
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+});
